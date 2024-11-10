98cee9662c53775d81f6d6c07222533b2076c05f
vibhav950 <vibhavtiwari.official@gmail.com>
2024-11-10 16:58:36 +0530

    Modify FundList styling
    Sorry for the autoformat

--- fund-ui/src/components/FundList.js
+++ fund-ui/src/components/FundList.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const FundList = ({ title, funds, onFundClick }) => {
-    return (
-        <div className="mb-8">
-            <h2 className="text-2xl font-bold text-gray-800 mb-4">{title}</h2>
-            <ul className="space-y-2">
-                {funds.map(([id, name, value]) => (
-                    <li
-                        key={id}
-                        onClick={() => onFundClick(id)}
-                        className="p-4 bg-white rounded-lg shadow hover:shadow-md hover:bg-gray-300 cursor-pointer transition-all duration-200 hover:translate-y-[-2px]"
-                    >
-                        {name} - {value}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="mb-8">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">{title}</h2>
+      <ul className="space-y-2">
+        {funds.map(([id, name, value]) => (
+          <li
+            key={id}
+            onClick={() => onFundClick(id)}
+            className="px-8 py-4 bg-white rounded-lg shadow hover:shadow-lg hover:bg-gray-300 cursor-pointer transition-all duration-300 ease-in-out hover:translate-y-[-4px] hover:scale-105 flex justify-between"
+          >
+            <span className="font-bold">{name}</span>
+            <span
+              className={`text-right font-bold ${
+                value > 0 ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              {value}
+              {"%"}
+              <span className="text-gray-400 text-sm">{" 1Y"}</span>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default FundList;
