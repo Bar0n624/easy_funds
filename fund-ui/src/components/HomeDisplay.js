cd95ee674a7c878e9a041d1b5171ad43d5fdf26c
Varun Shenoy <varun123shenoy@gmail.com>
2024-11-10 19:51:45 +0530

    changed homepage ui

--- fund-ui/src/components/HomeDisplay.js
+++ fund-ui/src/components/HomeDisplay.js
@@ -22,29 +22,32 @@ const HomeDisplay = ({ data, uid }) => {
                 {Object.entries(periods).map(([key, label]) => (
                     <div key={key} className="bg-white rounded-lg shadow hover:shadow-lg p-6 transition-all duration-200 hover:translate-y-[-2px]">
                         <h2 className="text-2xl font-semibold text-gray-800 mb-4">{label}</h2>
-                        <div className="overflow-x-auto">
-                            <table className="min-w-full">
-                                <thead className="bg-gray-50">
-                                <tr>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Company</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fund Name</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Performance (%)</th>
-                                </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                {data[key].map((fund, index) => (
-                                    <tr
-                                        key={index}
-                                        onClick={() => handleFundClick(fund[0])}
-                                        className="hover:bg-gray-50 cursor-pointer transition-colors"
+                        <div className="space-y-4">
+                            {data[key].map((fund, index) => (
+                                <li
+                                    key={index}
+                                    onClick={() => handleFundClick(fund[0])}
+                                    className="px-12 py-4 bg-white rounded-lg shadow hover:shadow-lg hover:bg-gray-300 cursor-pointer transition-all duration-300 ease-in-out hover:translate-y-[-4px] hover:scale-105 flex justify-between"
+                                    style={{ height: '80px' }}
+                                >
+                                    <div className="flex flex-col" style={{ maxWidth: '80%' }}>
+                                        <span className="text-left text-xl font-semibold truncate" style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                                            {fund[2]}
+                                        </span>
+                                        <span className="text-gray-400 text-sm truncate" style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                                            {fund[1]}
+                                        </span>
+                                    </div>
+                                    <span
+                                        className={`text-right font-bold text-lg ${
+                                            fund[3] > 0 ? "text-green-500" : "text-red-500"
+                                        }`}
+                                        style={{ maxWidth: '20%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
                                     >
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[1]}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[2]}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[3]}</td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
+                                        {fund[3] > 0 ? `+${fund[3]}` : fund[3]}%
+                                    </span>
+                                </li>
+                            ))}
                         </div>
                     </div>
                 ))}
@@ -53,31 +56,31 @@ const HomeDisplay = ({ data, uid }) => {
             <div className="bg-white rounded-lg shadow hover:shadow-lg p-6 transition-all duration-200 hover:translate-y-[-2px] mt-8">
                 <h2 className="text-2xl font-semibold text-gray-800 mb-4">Watchlist</h2>
                 {data.watchlist.length > 0 ? (
-                    <div className="overflow-x-auto">
-                        <table className="min-w-full">
-                            <thead className="bg-gray-50">
-                            <tr>
-                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fund ID</th>
-                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fund Name</th>
-                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Lifetime Return</th>
-                                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">1 Day Change</th>
-                            </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                            {data.watchlist.map((fund, index) => (
-                                <tr
-                                    key={index}
-                                    onClick={() => handleFundClick(fund[0])}
-                                    className="hover:bg-gray-50 cursor-pointer transition-colors"
-                                >
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[0]}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[1]}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[2]}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{fund[3]}</td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                    <div className="space-y-4">
+                        {data.watchlist.map((fund, index) => (
+                            <li
+                                key={index}
+                                onClick={() => handleFundClick(fund[0])}
+                                className="px-12 py-4 bg-white rounded-lg shadow hover:shadow-lg hover:bg-gray-300 cursor-pointer transition-all duration-300 ease-in-out hover:translate-y-[-4px] hover:scale-105 flex justify-between"
+                            >
+                                <div className="flex flex-col">
+                                    <span className="text-left text-xl font-semibold">
+                                        {fund[1]}
+                                    </span>
+                                    <span className="text-gray-400 text-sm">
+                                        {fund[0]}
+                                    </span>
+                                </div>
+                                <div className="flex flex-col text-right">
+                                    <span className="font-bold text-lg">
+                                        {fund[2] > 0 ? `+${fund[2]}` : fund[2]}%
+                                    </span>
+                                    <span className="text-gray-400 text-sm">
+                                        {fund[3]}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
                     </div>
                 ) : (
                     <p className="text-gray-500 text-center py-4">No funds in your watchlist.</p>
