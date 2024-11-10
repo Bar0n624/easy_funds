209bd4ec4e4f40101c8d4112a56e351a9ac0fe79
Varun Shenoy <varun123shenoy@gmail.com>
2024-11-10 20:46:29 +0530

    removed watchlist from homepage

--- fund-ui/src/components/HomeDisplay.js
+++ fund-ui/src/components/HomeDisplay.js
@@ -51,40 +51,6 @@ const HomeDisplay = ({ data, uid }) => {
                     </div>
                 ))}
             </div>
-
-            <div className="bg-white rounded-lg shadow hover:shadow-lg p-6 transition-all duration-200 hover:translate-y-[-2px] mt-8">
-                <h2 className="text-2xl font-semibold text-gray-800 mb-4">Watchlist</h2>
-                {data.watchlist.length > 0 ? (
-                    <div className="space-y-4">
-                        {data.watchlist.map((fund, index) => (
-                            <li
-                                key={index}
-                                onClick={() => handleFundClick(fund[0])}
-                                className="px-12 py-4 bg-white rounded-lg shadow hover:shadow-lg hover:bg-gray-300 cursor-pointer transition-all duration-300 ease-in-out hover:translate-y-[-4px] hover:scale-105 flex justify-between"
-                            >
-                                <div className="flex flex-col">
-                                    <span className="text-left text-xl font-semibold">
-                                        {fund[1]}
-                                    </span>
-                                    <span className="text-gray-400 text-sm">
-                                        {fund[0]}
-                                    </span>
-                                </div>
-                                <div className="flex flex-col text-right">
-                                    <span className="font-bold text-lg">
-                                        {fund[2] > 0 ? `+${fund[2]}` : fund[2]}%
-                                    </span>
-                                    <span className="text-gray-400 text-sm">
-                                        {fund[3]}
-                                    </span>
-                                </div>
-                            </li>
-                        ))}
-                    </div>
-                ) : (
-                    <p className="text-gray-500 text-center py-4">No funds in your watchlist.</p>
-                )}
-            </div>
         </div>
     );
 };
